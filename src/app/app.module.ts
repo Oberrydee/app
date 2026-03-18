1bf4b142ad8d75e7a05f6d242d7604636577134c
Diana AV <diana.adzoh-vinyo@agir-tech.fr>
2026-03-18 17:33:32 +0100

    add components and pages for cart, product, wishlist, home, and login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CartComponent } from './components/cart/cart.component';
+import { ProductComponent } from './components/product/product.component';
+import { WishlistComponent } from './components/wishlist/wishlist.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartComponent,
+    ProductComponent,
+    WishlistComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
