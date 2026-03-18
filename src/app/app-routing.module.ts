3703b2280fb54dc7750732d7b3d0ab8552b180d6
Diana AV <diana.adzoh-vinyo@agir-tech.fr>
2026-03-18 17:41:35 +0100

    refactor: restructure cart and authentication modules
    - Moved CartComponent, ProductComponent, and WishlistComponent to the new cart-module directory.
    - Created AuthenticationComponent with basic template and styles.
    - Added unit tests for AuthenticationComponent.
    - Created HTML and SCSS files for CartComponent, ProductComponent, and WishlistComponent with basic content.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { WishlistComponent } from './pages/wishlist/wishlist.component';
+import { ProductComponent } from './modules/cart-module/product/product.component';
+import { CartComponent } from './pages/cart/cart.component';
+import { LoginComponent } from './pages/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  //add routes to pages components
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'product', component: ProductComponent },
+  { path: 'wishlist', component: WishlistComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CartComponent } from './components/cart/cart.component';
-import { ProductComponent } from './components/product/product.component';
-import { WishlistComponent } from './components/wishlist/wishlist.component';
+import { CartComponent } from './modules/cart-module/cart/cart.component';
+import { ProductComponent } from './modules/cart-module/product/product.component';
+import { WishlistComponent } from './modules/cart-module/wishlist/wishlist.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 
