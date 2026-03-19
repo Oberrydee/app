470d34c45a1a86db6a88011e65a6002d5de73c2a
Diana AV <diana.adzoh-vinyo@agir-tech.fr>
2026-03-19 23:34:56 +0100

    feat: update home component to display current user information and improve navigation buttons

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { CurrentUser } from 'src/app/models/auth.models';
 import { AuthenticationService } from 'src/app/modules/authentication-module/authentication/authentication.service';
 
 @Component({
@@ -9,6 +11,7 @@ import { AuthenticationService } from 'src/app/modules/authentication-module/aut
 })
 export class HomeComponent implements OnInit {
   isAdmin$ = this.authenticationService.isAdmin();
+  currentUser$: Observable<CurrentUser | null> = this.authenticationService.currentUser$;
 
   constructor(
     private readonly authenticationService: AuthenticationService,
@@ -17,6 +20,7 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
     this.isAdmin$ = this.authenticationService.isAdmin();
+    this.currentUser$ = this.authenticationService.currentUser$;
   }
 
   logout(): void {
